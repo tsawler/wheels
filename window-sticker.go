53c3b54a01f06a30b4847529ad6a4b2b6682a00c
Trevor Sawler <trevor.sawler@gmail.com>
2020-06-15 10:38:31 -0300

    Window stickers complete

--- window-sticker.go
+++ window-sticker.go
@@ -122,8 +122,100 @@ func CreateWindowSticker(v clientmodels.Vehicle) (*gofpdf.Fpdf, error) {
 		pdf.SetX(157)
 		pdf.Write(0, fmt.Sprintf("Stock #: %s", v.StockNo))
 	} else {
-		// mvi select
-		t = importer.ImportPage(pdf, "./client/clienthandlers/pdf-templates/mv-plus-select.pdf", 1, "/MediaBox")
+		// mvi select plus
+		t = importer.ImportPage(pdf, "./client/clienthandlers/pdf-templates/mvi-plus-select.pdf", 1, "/MediaBox")
+		pdf.AddPage()
+		importer.UseImportedTemplate(pdf, t, 0, 0, 215.9, 0)
+
+		// write make/model/year/trim
+		pdf.SetFont("Arial", "BI", 24)
+		pdf.Write(0, fmt.Sprintf("%d %s %s %s", v.Year, v.Make.Make, v.Model.Model, v.Trim))
+
+		// price
+		pdf.SetX(162)
+		pdf.SetFont("Arial", "BI", 28)
+		pdf.Write(0, fmt.Sprintf(" $%s  ", humanize.Comma(int64(v.Cost))))
+
+		// write odometer
+		pdf.SetY(24)
+		pdf.SetFont("Arial", "B", 20)
+		pdf.Write(0, fmt.Sprintf("%s km", humanize.Comma(int64(v.Odometer))))
+
+		// write options
+		var col1 []clientmodels.VehicleOption
+		var col2 []clientmodels.VehicleOption
+		var col3 []clientmodels.VehicleOption
+
+		iteration := 1
+		for _, x := range v.VehicleOptions {
+			if iteration == 1 {
+				col1 = append(col1, *x)
+			} else if iteration == 2 {
+				col2 = append(col2, *x)
+			} else {
+				col3 = append(col3, *x)
+			}
+
+			iteration++
+			if iteration == 4 {
+				iteration = 1
+			}
+		}
+		var spacing float64
+		if len(col1) > 15 {
+			spacing = 2.3
+		} else if len(col1) > 10 {
+			spacing = 3.5
+		} else {
+			spacing = 4
+		}
+
+		// print first column
+		pdf.SetY(38)
+		for _, x := range col1 {
+			pdf.SetX(10)
+			pdf.SetFont("ZapfDingbats", "", 10)
+			pdf.SetTextColor(60, 169, 0)
+			pdf.Write(spacing, "4 ")
+			pdf.SetFont("CenturyGothic-Bold", "", 11)
+			pdf.SetTextColor(0, 0, 0)
+			pdf.MultiCell(0, spacing, x.OptionName, "", "L", false)
+			pdf.Ln(spacing)
+		}
+
+		// print second column
+		pdf.SetY(38)
+		for _, x := range col2 {
+			pdf.SetX(75)
+			pdf.SetFont("ZapfDingbats", "", 10)
+			pdf.SetTextColor(60, 169, 0)
+			pdf.Write(spacing, "4 ")
+			pdf.SetFont("CenturyGothic-Bold", "", 11)
+			pdf.SetTextColor(0, 0, 0)
+			pdf.MultiCell(0, spacing, x.OptionName, "", "L", false)
+			pdf.Ln(spacing)
+		}
+
+		// print third column
+		pdf.SetY(38)
+		for _, x := range col3 {
+			pdf.SetX(140)
+			pdf.SetFont("ZapfDingbats", "", 10)
+			pdf.SetTextColor(60, 169, 0)
+			pdf.Write(spacing, "4 ")
+			pdf.SetFont("CenturyGothic-Bold", "", 11)
+			pdf.SetTextColor(0, 0, 0)
+			pdf.MultiCell(0, spacing, x.OptionName, "", "L", false)
+			pdf.Ln(spacing)
+		}
+
+		// write Stock #
+		pdf.SetTextColor(0, 0, 0)
+		pdf.SetFont("CenturyGothic-Bold", "", 12)
+		pdf.SetY(250)
+		pdf.SetX(157)
+		pdf.Write(0, fmt.Sprintf("Stock #: %s", v.StockNo))
+
 	}
 
 	return pdf, nil
